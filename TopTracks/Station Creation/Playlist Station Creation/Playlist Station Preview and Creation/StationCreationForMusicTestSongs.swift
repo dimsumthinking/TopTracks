6f37538d3c2677a03384f3afb2b17fb47e7ce1c8
Daniel H Steinberg <inquiries@dimsumthinking.com>
2022-04-06 11:32:22 -0400

    One more file from last commit that got missed

---
+++ TopTracks/Station Creation/Playlist Station Creation/Playlist Station Preview and Creation/StationCreationForMusicTestSongs.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+import MusicKit
+import CoreData
+
+struct StationCreationForMusicTestSongs {
+  let playlist: Playlist
+  let songsInCategories: [SongInCategory]
+  
+  @FetchRequest(entity: TopTracksStation.entity(),
+                sortDescriptors: [NSSortDescriptor(key: "buttonPosition",
+                                                   ascending: true)])
+  private var stations: FetchedResults<TopTracksStation>
+  @EnvironmentObject private var topTracksStatus: TopTracksStatus
+}
+
+extension StationCreationForMusicTestSongs: View {
+  var body: some View {
+    Button("Subscribe to \n  \(playlist.name) + \(decorationForStationName)",
+           action: createStation)
+    .buttonStyle(.borderedProminent)
+    .padding(.vertical)
+  }
+}
+
+extension StationCreationForMusicTestSongs {
+  func createStation() {
+    let context = PersistenceController.newBackgroundContext
+    let _ = TopTracksStation(stationName: playlist.name + decorationForStationName,
+                             playlist: playlist,
+                             buttonPosition: stations.count,
+                             songsInCategories: songsInCategories,
+                             clock: RotationClock.hourWithSpice,
+                             context: context)
+    do {
+      topTracksStatus.isCreatingNew = false
+      try context.save()
+      print("tried to save \(playlist.name)")
+    } catch {
+      print("Not able to create a new station\n", error)
+    }
+  }
+  
+    private var decorationForStationName: String {
+      let numberWithSameStart = stations.map(\.stationName)
+        .filter{name in name.starts(with: playlist.name)}.count
+  
+      return numberWithSameStart == 0 ? "" : (" " + (numberWithSameStart + 1).description)
+    }
+}
+
+//struct StationCreationForCityChart_Previews: PreviewProvider {
+//  static var previews: some View {
+//    StationCreationForCityChart()
+//  }
+//}
