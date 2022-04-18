01feb40b6c3aefbc1f9581768a25e9d04c88d968
Daniel H Steinberg <inquiries@dimsumthinking.com>
2022-04-18 10:06:46 -0400

    Add elapsed and remaining time to the song scrubber

--- TopTracks/Launch/AppStatus/TopTracksStatus.swift
+++ TopTracks/Launch/AppStatus/TopTracksStatus.swift
@@ -1,8 +1,27 @@
 import Combine
 import MusicKit
+import Network
+import SwiftUI
 
 class TopTracksStatus: ObservableObject {
   @Published var isCreatingNew = false
   @Published var musicSubscription: MusicSubscription?
   @Published var needsAppSubscription = false
+  @AppStorage("requiresWiFi") private var requiresWiFi = false
+  private var pathMonitor = NWPathMonitor()
+//  init() {
+//    configurePathMonitor()
+//  }
 }
+//
+//extension TopTracksStatus {
+//  private func configurePathMonitor() {
+//    pathMonitor.pathUpdateHandler = {path in
+//      switch path.status {
+//      case:
+//      }
+//    }
+//  }
+//}
+
+
--- TopTracks/Players/Stations/Full Player View/SongScrubberView.swift
+++ TopTracks/Players/Stations/Full Player View/SongScrubberView.swift
@@ -4,13 +4,14 @@ import MusicKit
 struct SongScrubberView {
   let currentSong: Song
   @State private var currentTime: TimeInterval = 0
-  @State private var duration: TimeInterval = 5 * 60
+//  @State private var duration: TimeInterval = 5 * 60
 //  @Binding private(set) var isPlaying: Bool
   @State private var isDraggingSlider = false
 }
 
 extension SongScrubberView: View {
   var body: some View {
+    VStack {
     Slider(value: $currentTime, in: 0...(currentSong.duration ?? 5 * 60)){editing in
       if !editing {
       ApplicationMusicPlayer.shared.playbackTime = currentTime
@@ -18,7 +19,7 @@ extension SongScrubberView: View {
       isDraggingSlider = editing
 
     }
-    .padding()
+    .padding(.horizontal)
     .accentColor(.secondary)
     .padding(.horizontal)
       .onAppear {
@@ -26,8 +27,33 @@ extension SongScrubberView: View {
           try await startUpdatingCurrentTime()
         }
       }
+      if let duration = currentSong.duration {
+      HStack {
+        Text(timeInterval(for: currentTime))
+        Spacer()
+        Text(timeInterval(for: duration - currentTime))
+      }
+      .padding(.horizontal)
+      .font(.caption)
+      .padding(.horizontal)
+    }
+    }
   }
+}
 
+import Foundation
+
+fileprivate let formatter = DateComponentsFormatter()
+extension SongScrubberView {
+  private func timeInterval(for seconds: Double) -> String {
+    formatter.allowedUnits = [.minute, .second]
+    formatter.zeroFormattingBehavior = .pad
+    guard var formattedTime = formatter.string(from: seconds) else {return "-:--"}
+    if formattedTime.first == "0" {
+      formattedTime = String(formattedTime.dropFirst())
+    }
+    return formattedTime
+  }
 }
 
 extension SongScrubberView {
