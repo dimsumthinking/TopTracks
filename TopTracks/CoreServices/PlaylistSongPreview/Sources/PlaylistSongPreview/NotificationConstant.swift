3bf6d072dd620b8c14553a5fabab7e8fa7a99dad
Daniel H Steinberg <inquiries@dimsumthinking.com>
2023-04-23 08:18:40 -0400

    Pre-split of station creation

---
+++ TopTracks/CoreServices/PlaylistSongPreview/Sources/PlaylistSongPreview/NotificationConstant.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel Steinberg on 4/22/23.
+//
+
+import Foundation
---
+++ TopTracksTV/TopTracksTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTracksTVApp.swift
+//  TopTracksTV
+//
+//  Created by Daniel Steinberg on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TopTracksTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
